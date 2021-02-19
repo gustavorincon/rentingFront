cc52d2d102bb63e2f3a0454e20bcedddd8b808eb
julianGonzalezV <j.andres2087@gmail.com>
2021-02-18 22:32:25 -0500

    Adding user component manager

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, SignupComponent, HomeComponent],
   imports: [
     CommonModule
   ]
